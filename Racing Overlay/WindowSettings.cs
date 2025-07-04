6d92767e230b14082c161dc319e5e6c505174aab
bcox07 <brian.cox07@gmail.com>
2025-07-04 00:20:13 -0600

    Update iracing sdk package reference

--- Racing Overlay/WindowSettings.cs
+++ Racing Overlay/WindowSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Linq;
@@ -33,6 +34,44 @@ namespace IRacing_Standings
             { "XPos", "0" },
             { "YPos", "0" }
         };
+
+        public WindowSettings(AppSettingsSection appSettings = null)
+        {
+            if (appSettings != null)
+            {
+                StandingsSettings = new Dictionary<string, string>()
+                {
+                    { "Locked", appSettings.Settings["StandingsWindowLocked"].Value },
+                    { "XPos", appSettings.Settings["StandingsWindowXPos"].Value },
+                    { "YPos", appSettings.Settings["StandingsWindowYPos"].Value }
+                };
+                RelativeSettings = new Dictionary<string, string>()
+                {
+                    { "Locked", appSettings.Settings["RelativeWindowLocked"].Value },
+                    { "XPos", appSettings.Settings["RelativeWindowXPos"].Value },
+                    { "YPos", appSettings.Settings["RelativeWindowYPos"].Value }
+                };
+                FuelSettings = new Dictionary<string, string>()
+                {
+                    { "Locked", appSettings.Settings["FuelWindowLocked"].Value },
+                    { "XPos", appSettings.Settings["FuelWindowXPos"].Value },
+                    { "YPos", appSettings.Settings["FuelWindowYPos"].Value }
+                };
+                TireSettings = new Dictionary<string, string>()
+                {
+                    { "Locked", appSettings.Settings["LiveTrackWindowLocked"].Value },
+                    { "XPos", appSettings.Settings["TireWindowXPos"].Value },
+                    { "YPos", appSettings.Settings["TireWindowYPos"].Value }
+                };
+                LiveTrackSettings = new Dictionary<string, string>()
+                {
+                    { "Locked", appSettings.Settings["LiveTrackWindowLocked"].Value },
+                    { "XPos", appSettings.Settings["LiveTrackWindowXPos"].Value },
+                    { "YPos", appSettings.Settings["LiveTrackWindowYPos"].Value }
+                };
+            }
+           
+        }
 
         public void SaveSettings(string window, Dictionary<string, string> settings)
         {
