928a5f5ea11f6abe528835a30f69cf34c065e126
bcox07 <brian.cox07@gmail.com>
2026-03-11 23:05:53 -0600

    add hyundai logo

--- Racing Overlay/Models/CarLogo.cs
+++ Racing Overlay/Models/CarLogo.cs
@@ -7,38 +7,40 @@
         {
             switch (carPath.ToLower())
             {
-                case string s when s.StartsWith("acura"):
+                case string s when s.Contains("acura"):
                     return "acura-logo";
                 case "amvantagegt4":
                     return "aston-martin-logo";
-                case string s when s.StartsWith("audi"):
+                case string s when s.Contains("audi"):
                     return "audi-logo";
-                case string s when s.StartsWith("bmw"):
+                case string s when s.Contains("bmw"):
                     return "bmw-logo";
-                case string s when s.StartsWith("cadillac"):
+                case string s when s.Contains("cadillac"):
                     return "cadillac-logo";
                 case "chevyvettez06rgt3":
                     return "chevrolet-logo";
-                case string s when s.StartsWith("dallara"):
+                case string s when s.Contains("dallara"):
                     return "dallara-logo";
-                case string s when s.StartsWith("ferrari"):
+                case string s when s.Contains("ferrari"):
                     return "ferrari-logo";
-                case string s when s.StartsWith("ford"):
+                case string s when s.Contains("ford"):
                 case "fr500s":
                     return "ford-logo";
                 case string s when s.Contains("honda"):
                     return "honda-logo";
-                case string s when s.StartsWith("lamborghini"):
+                case string s when s.Contains("hyundai"):
+                    return "hyundai-logo";
+                case string s when s.Contains("lamborghini"):
                     return "lamborghini-logo";
-                case string s when s.StartsWith("mclaren"):
+                case string s when s.Contains("mclaren"):
                     return "mclaren-logo";
                 case "mx5 mx52016":
                     return "mazda-logo";
                 case "ligierjsp320":
                     return "ligier-logo";
-                case string s when s.StartsWith("mercedes"):
+                case string s when s.Contains("mercedes"):
                     return "mercedes-logo";
-                case string s when s.StartsWith("porsche"):
+                case string s when s.Contains("porsche"):
                     return "porsche-logo";
                 case string s when s.Contains("toyota"):
                     return "toyota-logo";
